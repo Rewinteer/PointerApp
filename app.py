0bef0d2ed74804f3d31f0e1a6e9ff2a4818eddb8
Alexander Kuksouski <akuksouski@gmail.com>
2023-05-08 15:56:29 +0300

    implemented id and attributes display

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import psycopg2
 import json
 
 app = Flask(__name__)
+app.debug = True
 
 def get_db_connection():
     conn = psycopg2.connect(
@@ -21,7 +22,7 @@ def index():
 def points():
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('SELECT id, ST_AsGeoJSON(ST_FlipCoordinates(location)) FROM points;')
+    cur.execute('SELECT id, ST_AsGeoJSON(ST_FlipCoordinates(location)), user_id, attributes FROM points;')
     rows = cur.fetchall()
 
     features = []
@@ -29,7 +30,9 @@ def points():
         feature = {
             "type": "Feature",
             "properties": {
-                "id": row[0]
+                "id": row[0], 
+                "user_id": row[2],
+                "attributes": row[3]
             },
             "geometry": json.loads(row[1])
         }
---
+++ static/js/getPoints.js
@@ -0,0 +1,22 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+fetch('/points')
+    .then(response => response.json())
+    .then(data => {
+        var points = L.geoJSON(data, {
+            onEachFeature: function (feature, layer) {
+                let popupContent = `<b>ID = ${feature.properties.id}</b>`;
+                const attributes = feature.properties.attributes;
+                for (const key in attributes) {
+                    popupContent += `</br>${key} = ${attributes[key]}`
+                }
+                layer.bindPopup(popupContent);
+                // ('${feature.properties.id}, ${feature.properties.attributes.name}')
+            }
+        });
+        points.addTo(map);
+    });
